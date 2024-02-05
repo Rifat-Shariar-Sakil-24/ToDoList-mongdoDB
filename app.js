1d28056a9233c2f3c827bbabe3d786d58aef1bcf
Rifat Shariar Sakil <rifatsakilmi1309@gmail.com>
2024-02-05 13:00:29 +0600

    push task to db

--- app.js
+++ app.js
@@ -5,16 +5,36 @@ app = express();
 app.set('view engine', 'ejs');
 app.use(bodyparser.urlencoded({extended:true}));
 
+
+// db connection
+const mongoose = require('mongoose');
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/ToDoList').then(console.log("db is connected"));
+}
+
+
 var lists = [];
 
 app.get("/",function(req,res){
     res.render('index', {Tasks : lists});
 })
+
+
+const taskSchema = new mongoose.Schema({
+    name: String
+  });
+  const TaskModel = mongoose.model('task', taskSchema);
+
 app.post("/",function(req,res){
     const formInput = req.body;
     const taskIs = formInput.nameOfTask;
     lists.push(taskIs);
-    console.log(lists);
+    //console.log(lists);
+
+    const eachTask = new TaskModel({ name: taskIs });
+    eachTask.save().then("pushed succesfully").catch();
+   // console.log(silence.name);
     res.redirect("/");
 })
 
